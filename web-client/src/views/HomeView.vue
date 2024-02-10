9228a9de419d42efe74604844fa767619423fd94
Tanapon Pitichat <perthpiti@gmail.com>
2024-02-10 13:34:55 +0700

    +move invalid package.json location +limit width of keyword search result section flex flex-wrap items-center justify-center

--- web-client/src/views/HomeView.vue
+++ web-client/src/views/HomeView.vue
@@ -3,7 +3,9 @@
     <div class="sticky top-0 z-50 w-full bg-white shadow-md">
       <AppNavbar></AppNavbar>
     </div>
-    <KeywordSearchResult></KeywordSearchResult>
+    <div class="max-w-screen-xl mx-auto p-4">
+      <KeywordSearchResult></KeywordSearchResult>
+    </div>
   </div>
 </template>
 
